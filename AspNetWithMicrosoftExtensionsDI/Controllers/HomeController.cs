7acd72b0af57644264507070d36d66b2f09b35ee
Christian Rodriguez <christianarg@outlook.com>
2021-03-14 20:20:55 +0100

    Add project files.

---
+++ AspNetWithMicrosoftExtensionsDI/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetWithMicrosoftExtensionsDI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMyInterface myInterface;
+
+        public HomeController(IMyInterface myInterface)
+        {
+            this.myInterface = myInterface;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = myInterface.Foo();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AspNetWithMicrosoftExtensionsDI/Global.asax.cs
@@ -0,0 +1,115 @@
+﻿using AspNetWithMicrosoftExtensionsDI.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+[assembly: PreApplicationStartMethod(typeof(AspNetWithMicrosoftExtensionsDI.MvcApplication), "InitModule")]
+
+namespace AspNetWithMicrosoftExtensionsDI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void InitModule()
+        {
+            RegisterModule(typeof(ServiceScopeModule));
+        }
+
+        protected void Application_Start()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceScopeModule.SetServiceProvider(services.BuildServiceProvider());
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DependencyResolver.SetResolver(new ServiceProviderDependencyResolver());
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<HomeController>();
+            services.AddTransient<IMyInterface, MyClass>();
+        }
+    }
+
+
+    internal class ServiceScopeModule : IHttpModule
+    {
+        private static ServiceProvider _serviceProvider;
+
+        public void Dispose()
+        {
+
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += Context_BeginRequest;
+            context.EndRequest += Context_EndRequest;
+        }
+
+        private void Context_EndRequest(object sender, EventArgs e)
+        {
+            var context = ((HttpApplication)sender).Context;
+            if (context.Items[typeof(IServiceScope)] is IServiceScope scope)
+            {
+                scope.Dispose();
+            }
+        }
+
+        private void Context_BeginRequest(object sender, EventArgs e)
+        {
+            var context = ((HttpApplication)sender).Context;
+            context.Items[typeof(IServiceScope)] = _serviceProvider.CreateScope();
+        }
+
+        public static void SetServiceProvider(ServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+    }
+
+    internal class ServiceProviderDependencyResolver : IDependencyResolver
+    {
+        public object GetService(Type serviceType)
+        {
+            if (HttpContext.Current?.Items[typeof(IServiceScope)] is IServiceScope scope)
+            {
+                return scope.ServiceProvider.GetService(serviceType);
+            }
+
+            throw new InvalidOperationException("IServiceScope not provided");
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            if (HttpContext.Current?.Items[typeof(IServiceScope)] is IServiceScope scope)
+            {
+                return scope.ServiceProvider.GetServices(serviceType);
+            }
+
+            throw new InvalidOperationException("IServiceScope not provided");
+        }
+    }
+
+    public interface IMyInterface
+    {
+        string Foo();
+    }
+
+    public class MyClass : IMyInterface
+    {
+        public string Foo()
+        {
+            return "Bar";
+        }
+    }
+}
