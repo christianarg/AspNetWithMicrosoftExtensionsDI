0e4080bb2e190ed13a08494d9e9f3648001b7816
Christian Rodriguez <christianarg@gmail.com>
2023-01-17 18:34:42 +0100

    test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,52 @@
+using Microsoft.Extensions.DependencyInjection;
+using Unity;
+using Unity.Microsoft.DependencyInjection;
+
 namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void UnityExtensionsBuildServiceProviderTest()
         {
+            var unityContainer = new UnityContainer();
+            unityContainer.RegisterType<IMyInterface, MyClass>();
+            unityContainer.RegisterType<IMyInterface, MyClassNamed>("paco");
+
+            var services = new ServiceCollection();
+            services.AddHttpClient();
+
+            var serviceProvider = services.BuildServiceProvider(unityContainer);    // Al llamar a esto se hace la "magia". Lo que hay registrado en ServiceProvider lo registra también en Unity
+
+            var httpClientThroughServiceProvider = serviceProvider.GetService<IHttpClientFactory>();
+            Assert.IsNotNull(httpClientThroughServiceProvider);
+
+            var httpClientThroughUnity = unityContainer.Resolve<IHttpClientFactory>();
+            Assert.IsNotNull(httpClientThroughUnity);
+
+
+        }
+    }
+
+    public interface IMyInterface
+    {
+        string Foo();
+    }
+
+    public class MyClass : IMyInterface
+    {
+        public string Foo()
+        {
+            return "Bar";
+        }
+    }
+
+    public class MyClassNamed : IMyInterface
+    {
+        public string Foo()
+        {
+            return "BarNamed";
         }
     }
 }
